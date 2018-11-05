d580eb37973b994aee8e1416108a19374e26eef8
Artem Shubovych <ashubovych@atlassian.com>
2018-11-05 11:06:53 +1100

    Added GLFW

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <vulkan/vulkan.h>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <stdexcept>
 #include <functional>
@@ -7,24 +10,44 @@
 #include <memory>
 
 class Application {
+protected:
+    GLFWwindow *window;
+
 public:
     void run() {
         initVulkan();
+        createWindow();
+
         mainLoop();
+
         cleanup();
     }
 
 private:
     void initVulkan() {
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
+        std::cout << extensionCount << " extensions supported" << std::endl;
+    }
+
+    void createWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
     }
 };
 
