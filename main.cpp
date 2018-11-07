060bde56d27120d52bdd0bb9c69b3b6024a3b9bb
Artem Shubovych <ashubovych@atlassian.com>
2018-11-07 20:14:43 +1100

    Revert "GLFW => SDL2"
    This reverts commit 3dda9c5a1c7b03edffab1391838d0e0155fa1862.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <vulkan/vulkan.h>
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -15,12 +15,10 @@
 #include <functional>
 #include <cstdlib>
 #include <memory>
-#include <vector>
 
 class Application {
 protected:
-    SDL_Window *window;
-    VkInstance instance;
+    GLFWwindow *window;
 
 public:
     void run() {
@@ -34,89 +32,35 @@ public:
 
 private:
     void initVulkan() {
-        createVkInstance();
-
-        checkVkExtensions();
-    }
-
-    void createVkInstance() {
-        VkApplicationInfo appInfo = {};
-        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
-
-        VkInstanceCreateInfo createInfo = {};
-        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        createInfo.pApplicationInfo = &appInfo;
-
-        createInfo.enabledLayerCount = 0;
-
-        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
-
-        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-            throw std::runtime_error("Failed to create Vulkan instance");
-        }
-    }
-
-    void checkVkExtensions() {
         uint32_t extensionCount = 0;
-        // vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-        SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-        // std::vector<VkExtensionProperties> extensions(extensionCount);
+        std::cout << extensionCount << " extensions supported" << std::endl;
 
-        // vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        glm::mat4 matrix;
+        glm::vec4 vec;
+        auto test = matrix * vec;
 
-        std::vector<const char*> extensions(extensionCount);
-
-        SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensions.data());
-
-        std::cout << "Available " << extensionCount << " Vulkan extensions:" << std::endl;
-
-        for (const auto& extension : extensions) {
-            // std::cout << "\t" << extension.extensionName << std::endl;
-            std::cout << "\t" << extension << std::endl;
-        }
+        std::cout << "matrix x vector = " << glm::to_string(test) << std::endl;
     }
 
     void createWindow() {
-        SDL_Surface* screenSurface = nullptr;
+        glfwInit();
 
-        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-            std::cout << "SDL could not initialize. SDL_Error: " << SDL_GetError() << std::endl;
-            throw std::runtime_error("SDL could not initialize");
-        }
-
-        window = SDL_CreateWindow("Vulkan Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-
-        if (window == nullptr) {
-            std::cout << "Window could not be created. SDL_Error: " << SDL_GetError() << std::endl;
-            throw std::runtime_error("Can not open SDL window");
-        }
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
     }
 
     void mainLoop() {
-        SDL_Event e;
-        bool isRunning = true;
-
-        while (isRunning) {
-            while (SDL_PollEvent(&e)) {
-                if (e.type == SDL_QUIT) {
-                    isRunning = false;
-                }
-            }
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
         }
     }
 
     void cleanup() {
-        vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
 
-        SDL_DestroyWindow(window);
-
-        SDL_Quit();
+        glfwTerminate();
     }
 };
 
