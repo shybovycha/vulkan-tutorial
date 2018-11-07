f0336a1793ef96cde5f3f5802bb04be5dec23d9a
Artem Shubovych <ashubovych@atlassian.com>
2018-11-07 21:20:52 +1100

    Creating physical device

--- main.cpp
+++ main.cpp
@@ -37,6 +37,7 @@ private:
     void initVulkan() {
         createVkInstance();
         checkVkExtensions();
+        createVkPhysicalDevice();
     }
 
     void createVkInstance() {
@@ -79,6 +80,45 @@ private:
         for (const auto& extension : extensions) {
             std::cout << " - " << extension.extensionName << std::endl;
         }
+    }
+
+    void createVkPhysicalDevice() {
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+        if (deviceCount == 0) {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        std::cout << deviceCount << " Vulkan devices found" << std::endl;
+
+        for (const auto& device : devices) {
+            if (isVkDeviceSuitable(device)) {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE) {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+    }
+
+    bool isVkDeviceSuitable(VkPhysicalDevice device) {
+        VkPhysicalDeviceProperties deviceProperties;
+        vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+        VkPhysicalDeviceFeatures deviceFeatures;
+        vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+        // return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && deviceFeatures.geometryShader;
+        // return deviceFeatures.geometryShader;
+        return true;
     }
 
     void createWindow() {
